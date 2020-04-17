593084f4bb6cbbe06e5600fad0a85fb8ca0572b3
huingPC <li734462972@live.cn>
2020-04-17 18:29:32 +0800

    文件夹整理

--- public/template/web/lte/public/static/js/tool.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-    alert('11111');
-    $('.pagination').addClass("pagination-sm m-0 float-right");
-    $('.pagination li').addClass("page-item");
-    $('.pagination li span').addClass("page-link");
-    $('.pagination li a').addClass("page-link");
-})
