93fe9f0e6227bc8aa9c3edd78b5b59fd6515ed48
huingPC <li734462972@live.cn>
2020-04-25 14:43:46 +0800

    优化上传

--- app/UserBaseController.php
+++ app/UserBaseController.php
@@ -59,7 +59,7 @@ class UserBaseController extends WebBaseController
         }
         $userInfo = DB::name('user')->where('id',$userId)->find();
         if (empty($userInfo)) {
-            header('location:/user/centor');exit();
+            redirect(getDomain().'/user/user/centor')->send();exit;
         }
         $userInfo['myself'] = $mySelf;
         return $userInfo;
