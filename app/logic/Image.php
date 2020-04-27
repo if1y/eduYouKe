42dba9de3fc1c3152576357bbf0c9cda2f7c6b26
huingPC <li734462972@live.cn>
2020-04-27 23:06:13 +0800

    模板修改

--- app/logic/Image.php
+++ app/logic/Image.php
@@ -74,8 +74,8 @@ class Image
     //图片本地上传
     public function ImageLocalUpload($file, $param)
     {
-        return \think\facade\Filesystem::putFile( 'topic', $file, 'md5');
-        // return \think\facade\Filesystem::disk('public')->putFile('topic', $file);
+        // return \think\facade\Filesystem::putFile( 'topic', $file, 'md5');
+        return \think\facade\Filesystem::disk('public')->putFile('topic', $file);
     }
 
     //上传至阿里云
